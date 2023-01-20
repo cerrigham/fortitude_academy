d3d133546706711b2dfa45d76876318092934471
cerrigham <cerringham@gmail.com>
2023-01-20 10:26:06 +0100

    refactoring CompanyUtility methods

--- src/main/java/it/proactivity/utility/CompanyUtility.java
+++ src/main/java/it/proactivity/utility/CompanyUtility.java
@@ -1,15 +1,12 @@
 package it.proactivity.utility;
 
 import it.proactivity.model.Company;
-import it.proactivity.model.CompanyLocation;
 import org.hibernate.Session;
-import org.hibernate.annotations.Check;
 import org.hibernate.query.Query;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import javax.swing.event.ListDataEvent;
 import java.util.List;
 
 import static it.proactivity.utility.SessionUtility.checkSession;
@@ -17,37 +14,8 @@ import static it.proactivity.utility.SessionUtility.endSession;
 
 public class CompanyUtility {
 
-    private static Company createACompany(String name) {
-        if (name == null) {
-            return null;
-        }
-        Company company = new Company();
-        company.setName(name);
-        return company;
-    }
-
-    private static List<Company> checkACompany(Session session, Long id) {
-        CriteriaBuilder cb = session.getCriteriaBuilder();
-        CriteriaQuery<Company> cr = cb.createQuery(Company.class);
-        Root<Company> root = cr.from(Company.class);
-        cr.select(root).where(cb.equal(root.get("id"), id));
-
-        Query<Company> query = session.createQuery(cr);
-        List<Company> companyList = query.getResultList();
-        if (companyList.size() > 1) {
-            return null;
-        }
-        return companyList;
-    }
-
-    private static void checkForUpdate(Company company, String name) {
-        if (name == null) {
-            company.setName(name);
-        }
-    }
-
-    public static Boolean insertOrUpdateCompany(Session session, String name, Long id) {
-        if (session == null || name == null) {
+    public static Boolean insertOrUpdateCompany(Session session, Long id, String name) {
+        if (session == null || name == null) { //TODO check isEmpty
             return false;
         }
         checkSession(session);
@@ -57,35 +25,69 @@ public class CompanyUtility {
             session.persist(company);
             endSession(session);
             return true;
-        }
-        List<Company> companyList = checkACompany(session, id);
-        if (companyList.size() == 0) {
-            endSession(session);
         } else {
-            Company company = companyList.get(0);
-            checkForUpdate(company, name);
-            endSession(session);
-            return true;
+            Company company = getCompanyById(session, id);
+            if (company == null) {
+                endSession(session);
+                return null;
+            } else {
+                checkForUpdate(company, name);
+                session.persist(session);
+                endSession(session);
+                return true;
+            }
         }
-        return false;
     }
 
     public static Boolean deleteACompany(Session session, Long id) {
         if (session == null || id == null) {
             return false;
         }
+
         checkSession(session);
         final String query = "SELECT c " +
                 "FROM Company c " +
                 "WHERE c.id = :id";
         Query<Company> companyQuery = session.createQuery(query).setParameter("id", id);
         List<Company> companyList = companyQuery.getResultList();
-        if (companyList.size() > 1 || companyList == null) {
+        if (companyList == null || companyList.size() > 1) {
             endSession(session);
             return false;
         }
         session.delete(companyList.get(0));
         endSession(session);
         return true;
+    }
+
+    private static Company createACompany(String name) {
+        Company company = new Company();
+        company.setName(name);
+
+        return company;
+    }
+
+    private static Company getCompanyById(Session session, Long id) {
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Company> cr = cb.createQuery(Company.class);
+        Root<Company> root = cr.from(Company.class);
+        cr.select(root).where(cb.equal(root.get("id"), id));
+
+        Query<Company> query = session.createQuery(cr);
+        List<Company> companyList = query.getResultList();
+
+        if (companyList == null) {
+            return null;
+        } else if (companyList.isEmpty()) {
+            return null;
+        } else if (companyList.size() > 1) {
+            return null; //TODO call execption
+        } else
+            return companyList.get(0);
+    }
+
+    private static void checkForUpdate(Company company, String name) {
+        if (name != null || !name.isEmpty()) {
+            company.setName(name);
+        }
     }
 }
